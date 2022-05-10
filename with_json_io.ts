02e6583ce8bd88d3df0ca4b08214e94b36a77719
albnnc <albnnc@gmail.com>
2022-05-11 01:15:23 +0300

    feat: JSON IO handles NULL pointers

--- with_json_io.ts
+++ with_json_io.ts
@@ -11,6 +11,9 @@ export function withJsonIo(pointerIoFn: PointerIoFn): JsonIoFn {
   const jsonIoFn: JsonIoFn = async (jsonInput) => {
     const pointerInput = encoder.encode(JSON.stringify(jsonInput));
     const pointerOutput = await pointerIoFn(pointerInput);
+    if (!pointerOutput.value) {
+      throw new Error("Got null pointer");
+    }
     const jsonOutput = JSON.parse(
       new Deno.UnsafePointerView(pointerOutput).getCString()
     );
